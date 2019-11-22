ac2605cefb3993ab1b019899cd6bdc5b6db4fe11
Kaklikyan <armendevelop@gmail.com>
2019-11-22 15:05:43 +0400

    overall changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const imageMin = require('gulp-imagemin');
-const cssMinify = require('gulp-clean-css');
-const rename = require('gulp-rename');
-const { series } = require('gulp');
+const { watch, series } = require('gulp');
 
 
 gulp.task('style', function(done) {
@@ -14,16 +12,6 @@ gulp.task('style', function(done) {
             .pipe(browserSync.stream())
             done()
 });
-
-gulp.task('cleanCss', function(done) {
-  gulp.src('css/styles.css')
-  .pipe(cssMinify())
-  .pipe(rename({
-    suffix: '.min'
-  }))
-  .pipe(gulp.dest('css'))  
-  done()
-})
 
 gulp.task('imgMinify', function(done) {
     gulp.src('images/*')
@@ -41,4 +29,9 @@ gulp.task('server', function(done) {
   done()
 })
 
-gulp.task('default', series(['style', 'imgMinify', 'server']))
+
+const watchFiles = () => {
+  watch(['./scss/*.scss', './*.html'], series(['style', 'imgMinify']))
+}
+
+gulp.task('default', series(['style', 'imgMinify', 'server', watchFiles]))
